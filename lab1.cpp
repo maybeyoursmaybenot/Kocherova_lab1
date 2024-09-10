db3daff137e8221e1396eb2ea3e6a32d132ebf26
Anastasia <30.infonet@gmail.com>
2024-09-10 11:21:46 +0300

    Start describing classes for pipeline and CS
    Parameters and calling constructor

--- lab1.cpp
+++ lab1.cpp
@@ -1,6 +1,39 @@
 using namespace std;
 #include <iostream>
 
-int main() {
+class Pipeline {
+private:
+	string marker;
+	int length;
+	int diameter;
+	bool state;
+public:
+	Parameters_Pipeline(string marker, int length, int diameter, bool state);
 
+	string GetMarker() const;
+	int GetLength() const;
+	int GetDiameter() const;
+	bool GetState() const;
+};
+
+class CompressorStation {
+private:
+	string name;
+	int dep_count;
+	int dep_count_active;
+	int efficiency_k;
+public:
+	Parameters_CompressorStation(string name, int dep_count, int dep_count_active, float efficiency_k);
+
+	string GetName() const;
+	int GetDepCount() const;
+	int GetDepCountActive() const;
+	int GetEfficiencyK() const;
+};
+
+int main() {
+	/*while 1 {
+		
+	}*/
+	return 0;
 }
